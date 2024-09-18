2ce040e65920ab145664d5aaffbfe64ac85b5a1e
santiagu-gap <santiagu.gap@gmail.com>
2024-09-18 15:47:00 -0400

    Logic for LED in application.py, we created some routes for /on and /off (we renamed the main.py to application.py) now we're just missing the HTML for the dashboard, aka on.html and off.html - for more info check the video in the instagram groupchat or ask questions in the insta groupchat

--- application.py
+++ application.py
@@ -3,6 +3,22 @@ import datetime
 
 app = Flask(__name__)
 
+led_pin = (17)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.output(led_pin, LOW)
+
+@app.route('/on')
+def on():
+  GPIO.output(led_pin, HIGH)
+  return render_template('on.html')
+
+@app.route('/off')
+def off():
+  GPIO.output(led_pin, LOW)
+  return render_template('off.html')
+
 @app.route('/')
 
 def index():
@@ -16,4 +32,4 @@ def index():
 
 #REPPLACE IP Address with ur Pi address
 if __name__ == '__main__':
-    app.run(host='IP Address', port='8000')
+    app.run(host='0.0.0.0', port='8000')
